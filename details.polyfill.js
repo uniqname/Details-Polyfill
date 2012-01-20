228cf8987936e9d5b3c0248315f4d522f62f53f1
Cory <oh.wise.man@gmail.com>
2012-01-20 17:13:41 -0500

    Attempting to minimize the possibility of css conflicts

--- details.polyfill.js
+++ details.polyfill.js
@@ -4,23 +4,23 @@
     var i,
         idCount = 0,
         detailsID,
-        detailElem,
+        detailsElem,
         summaryElem,
         height, //height of the summary element
-        headElem = (doc.getElementsByTagName('head')) ? doc.getElementsByTagName('head')[0] : doc.documentElement,
-        bodyElem = (doc.getElementsByTagName('body')) ? doc.getElementsByTagName('body')[0] : doc.documentElement,
-        detailElems = doc.getElementsByTagName('details'),
+        rootNode = doc.documentElement,
+        headElem = doc.getElementsByTagName('head')[0] || rootNode,
+        bodyElem = doc.getElementsByTagName('body')[0] || rootNode,
+        detailsElems = doc.getElementsByTagName('details'),
         detailStyleTag = doc.createElement('style'),
-        rules = 'details { display: block; overflow:hidden; } \n' +
+        rules = 'details { display: block; } \n' +
                 'details[open] { height: auto; } \n' +
-                'summary { display: block; cursor: pointer; }',
-                /* TODO: 
-                 *      Technically, a summary element has a "Phrasing content" model and should be displayed inline.
-                 *      see http://dev.w3.org/html5/spec/Overview.html#the-summary-element,
-                 *          http://dev.w3.org/html5/spec/Overview.html#phrasing-content
+                'details > summary:first-child { visibility: visible; cursor: pointer; display:block; margin-bottom: 9999em; } \n' +
+                'details[open] > summary:first-child { margin-bottom: 0; display: inline; }',
+                /* Technically, a summary element has a "Phrasing content" model and should be displayed inline.
+                 * see http://dev.w3.org/html5/spec/Overview.html#the-summary-element,
+                 *     http://dev.w3.org/html5/spec/Overview.html#phrasing-content
                  *
-                 *      Implimenting that with CSS creates a problem of hiding any content that may be on the same line
-                 *      as the summary element. Thought needs to be given as to how to adhere to the spec more closely.
+                 * FYI: Chrome currently and incorrectly treats the <summary> element as block level.
                  */
         addRule = function (styleTag, rule) {
             rule = '\n' + rule;
@@ -31,54 +31,51 @@
             }
         },
         toggle = function (e) {
-            var detailElmnt;
-            /* When a <summary> element is clicked the parent <details>
-             * element's "open" attribute needs to be toggled.
-            */
+            var detailsElmnt;
+            /* When a <summary> element is clicked the parent <details> element's "open"
+             * attribute needs to be toggled to maintain the attribute's reflective nature.
+             * see http://dev.w3.org/html5/spec/Overview.html#attr-details-open
+             */
             if (e.target.nodeName.toLowerCase() === 'summary') {
-                detailElmnt = e.target;
-                while (detailElmnt.nodeName.toLowerCase() !== 'details') {
-                    detailElmnt = detailElmnt.parentNode;
+                detailsElmnt = e.target;
+                while (detailsElmnt.nodeName.toLowerCase() !== 'details') {
+                    detailsElmnt = detailsElmnt.parentNode;
                     //Break if we get to the root node without finding a details element.
-                    if (detailElmnt === bodyElem) {
-                        detailElmnt = null;
+                    if (detailsElmnt === bodyElem) {
+                        detailsElmnt = null;
                         break;
                     }
                 }
-                if (detailElmnt) {
-                    /* Via http://dev.w3.org/html5/spec/Overview.html#attr-details-open
-                     * 
-                     *      The open content attribute is a boolean attribute. If present, it indicates that both the 
-                     *      summary and the additional information is to be shown to the user. If the attribute is  
-                     *      absent, only the summary is to be shown.
-                     * 
-                     * The open attribute is reflective and should always accurately represent the current state of 
-                     * the element
-                    */
-                    if (detailElmnt.getAttribute('open')) {
-                        detailElmnt.removeAttribute('open');
-                    } else { detailElmnt.setAttribute('open', 'open'); }
+                if (detailsElmnt) {
+                    if (detailsElmnt.getAttribute('open')) {
+                        detailsElmnt.removeAttribute('open');
+                    } else { detailsElmnt.setAttribute('open', 'open'); }
                 }
             }
         };
 
     addRule(detailStyleTag, rules);
-    for (i = 0; i < detailElems.length; i++) {
-        detailElem = detailElems[i];
+    for (i = 0; i < detailsElems.length; i++) {
+        detailsElem = detailsElems[i];
         detailsID = 'd' + (idCount++);
-        detailElem.setAttribute('data-detailsID', detailsID);
-        /* For details to toggle, the details element needs to have at least 
-         * one summary element, but only the first one is of any concern.
+        detailsElem.setAttribute('data-detailsID', detailsID);
+        /* The spec expects the functional <summary> element to be the first child node of a <details> element.
+         * In practice, it appears the first child <summary> element of a <detials> element is enlisted as the
+         * functional <summary> element and displayed as though it were the first child. For our purposes, we will 
+         * do that explicitly. Additionaly, If a <summary> element does not exist, a default toggle is provided.
          */
-        summaryElem = detailElem.getElementsByTagName('summary')[0];
+        summaryElem = detailsElem.getElementsByTagName('summary')[0];
         if (!summaryElem) {
             summaryElem = doc.createElement('summary');
             summaryElem.appendChild(doc.createTextNode('Details'));
-            detailElem.insertBefore(summaryElem, detailElem.firstChild);
+            detailsElem.insertBefore(summaryElem, detailsElem.firstChild);
+        } else if ( summaryElem !== detailsElem.firstChild) {
+            detailsElem.removeChild(summaryElem);
+            detailsElem.insertBefore(summaryElem, detailsElem.firstChild);
         }
         height = summaryElem.offsetHeight;
         addRule(detailStyleTag, 'details[data-detailsid="' + detailsID + '"] { height: ' + height + 'px; }\n' +
-                            'details[data-detailsid="' + detailsID + '"][open] { height: auto; }');
+                                'details[data-detailsid="' + detailsID + '"][open] { height: auto; }');
     }
     /* The inserted stylesheet needs to be first so as to have a minimal cascading coeffecient. 
      * The polyfill only adds default styling and should not interfere with other style rules.
